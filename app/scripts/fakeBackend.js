6551ee87c2ca7a7fb0a5bf88678aba12cadd8ac1
artem <lineartem@gmail.com>
2015-11-09 16:37:51 +0200

    fake backend support added

--- app/scripts/fakeBackend.js
+++ app/scripts/fakeBackend.js
@@ -5,16 +5,20 @@ define(
     ['./modules/App', 'lodash', './globalConfig'],
     function (App, _, appConfig, routes) {
         'use strict';
+
+        //if there is no reason to use fake backend then cancel
+        if (!appConfig.useFakeAPIService) {
+            return;
+        }
         App.run([
             '$httpBackend',
             function ($httpBackend) {
-                if (appConfig.useFakeAPIService) {
-                    $httpBackend.whenGET('/service/ping').respond(function (method, url, data) {
-                        return 'Ping successful!';
-                    });
-                    // Pass through everything else
-                    $httpBackend.whenGET(/.*/).passThrough();
-                    $httpBackend.whenPOST(/.*/).passThrough();
-                }
+                $httpBackend.whenGET('/service/ping').respond(function (method, url, data) {
+                    return 'Ping successful!';
+                });
+                // Pass through everything else
+                $httpBackend.whenGET(/.*/).passThrough();
+                $httpBackend.whenPOST(/.*/).passThrough();
+
             }])
     });
